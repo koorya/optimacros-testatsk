1c1a350381d0453662621a5fd000d3ab21d2404a
kirill <Puaktopblm@gmail.com>
2022-12-25 12:01:53 +0500

    Сделать запуск сервера в контейнере

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 import { app } from "./app/app";
 
 const main = async () => {
-  await mongoose.connect(`mongodb://root:example@127.0.0.1:27017`);
+  const mongoHost = process.env.MONGO_HOST
+    ? process.env.MONGO_HOST
+    : "127.0.0.1";
+  await mongoose.connect(`mongodb://root:example@${mongoHost}:27017`);
 
   app.listen(5000, () => console.log("server listen on port 5000"));
 };
